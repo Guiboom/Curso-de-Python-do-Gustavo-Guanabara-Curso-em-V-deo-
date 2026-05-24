e7c077222704f01818ab13d206fa31d6ae59fc4e
Guilherme Mantoanelli <gbmsom@gmail.com>
2026-05-24 16:29:24 -0300

    Update ex101.py

--- pythonExercícios/Execícios/Execícios Mundo 3/ex101.py
+++ pythonExercícios/Execícios/Execícios Mundo 3/ex101.py
@@ -1,1 +1,22 @@
-# ex101.py
+from datetime import date
+
+def voto(ano):
+    """-> Verifica se pode, deve ou não pode votar.
+        :param ano: ano de nascimento
+        :return: NEGADO, OPCIONAL, OBRIGATÓRIO
+        """
+    ano_atual = date.today().year
+    idade = ano_atual - ano
+
+    if idade < 16:
+        return 'NEGADO'
+    
+    elif idade < 18 or idade > 70 :
+        return 'OPCIONAL'
+    
+    elif idade < 69:
+        return 'OBRIGATÓRIO'
+    
+ano = int(input("Em que ano você nasceu?: "))
+status = voto(ano)
+print(f'Com base na sua idade, o seu voto é: {status}')
