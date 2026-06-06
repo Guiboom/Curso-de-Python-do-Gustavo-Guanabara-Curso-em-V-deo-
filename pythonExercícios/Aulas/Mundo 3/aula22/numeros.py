6ae41ca3274ea6e54ae196eeba222e82d2489492
Guilherme Mantoanelli <gbmsom@gmail.com>
2026-06-06 14:25:14 -0300

    update(not complet)

---
+++ pythonExercícios/Aulas/Mundo 3/aula22/numeros.py
@@ -0,0 +1,9 @@
+import uteis
+
+num = int(input("Digite um número: "))
+fat = fatorial(num)
+dob = dobro(num)
+tri = triplo(num)
+print(f"O fatorial de {num} é {fat}")
+print(f"O dobro de {num} é {dob}")
+print(f"O triplo de {num} é {tri}")
